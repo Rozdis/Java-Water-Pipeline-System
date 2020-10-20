91592d360e4741d1fb6fa06dba9271f9b76cda07
Rozdis <64463700+Rozdis@users.noreply.github.com>
2020-10-20 23:14:58 +0300

    Update Controller.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -27,7 +27,7 @@ import java.util.List;
 
 public class Controller {
     static final String JDBC_DRIVER = "org.h2.Driver";
-    static final String DB_URL = "jdbc:h2:tcp://localhost/~/test";
+    static final String DB_URL = "jdbc:h2:./test";
 
     static final String USER = "sa";
     static final String PASS = "";
